8f92818d58cfc90c4f158bf1b8fc706d549aacbe
Gustavo Valverde <g.valverde02@gmail.com>
2024-06-28 17:25:42 -0400

    fix(deploy): fix db state and bucket sync

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Restore the database if it does not already exist.
-if [ -f /data/db ]; then
+if [ -f "${DB_PATH}" ]; then
 	echo "Database already exists, skipping restore"
 else
 	echo "No database found, restoring from replica if exists"
-	litestream restore -if-replica-exists -o /data/db "${REPLICA_URL}"
+	litestream restore -if-replica-exists -o "${DB_PATH}" "${REPLICA_URL}"
 fi
 
 # Run litestream with your app as the subprocess.
