4569b34245d9da24ab707ccc5106be06b1e141b7
Gustavo Valverde <g.valverde02@gmail.com>
2024-07-11 14:51:13 +0100

    fix(startup): use correct logic

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
 
-# Restore the database if it does not already exist.
-if [[ -f "${DB_PATH}" || "${UPTIME_KUMA_DB_TYPE}" != 'mariadb' ]]; then
-	echo "Database already exists, skipping restore"
-else
-	echo "No database found, restoring from replica if exists"
-	litestream restore -if-replica-exists -o "${DB_PATH}" "${REPLICA_URL}"
-fi
-
 if [[ "${UPTIME_KUMA_DB_TYPE}" != 'mariadb' ]]; then
 	exec node server/server.js
 else
+	# Restore the database if it does not already exist.
+	if [[ -f "${DB_PATH}" ]]; then
+		echo "Database already exists, skipping restore"
+	else
+		echo "No database found, restoring from replica if exists"
+		litestream restore -if-replica-exists -o "${DB_PATH}" "${REPLICA_URL}"
+	fi
 	# Run litestream with your app as the subprocess.
 	exec litestream replicate -exec "node server/server.js"
 fi
